f7db82475f39401497bc7d45ddfc7f17f4e7d80b
Sunny Pun <sunnyppsp@gmail.com>
2022-11-14 00:58:52 +0800

    :sparkles: start with bottom while allowing hash locations

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Enhancements (This is an aggregated list, to be done in the future)
 
-- Upward scrolling from the start
 - Responsive for desktop
 - Cooling Pad story
 - Gears photo
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import styled, { ThemeProvider } from "styled-components"
-import i18n from "./i18n"
 import { defaultTheme } from "./theme/theme"
 import AboutMe from "./component/AboutMe"
 import Experience from "./component/Experience"
@@ -8,6 +7,7 @@ import Gears from "./component/Gears"
 import Menu from "./component/Menu"
 import { withTranslation } from "react-i18next"
 import LifeGoals from "./component/LifeGoals"
+import { useEffect, useRef } from "react"
 
 const Container = styled.div`
 display: flex;
@@ -18,7 +18,29 @@ overflow-x: clip;
 padding-bottom: ${props => props.theme.size.menu};
 `
 
-function App({ t }: { t: any }) {
+function App() {
+  const scrolledRef = useRef(false)
+  const { hash } = window.location
+
+  // modified from: https://ericdcobb.medium.com/scrolling-to-an-anchor-in-react-when-your-elements-are-rendered-asynchronously-8c64f77b5f34
+  useEffect(() => {
+    if (!scrolledRef.current) {
+      if (!hash) {
+        window.scrollTo({
+          top: document.body.scrollHeight,
+        })
+        return
+      }
+
+      const id = hash.replace('#', '')
+      const element = document.getElementById(id)
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' })
+        scrolledRef.current = true
+      }
+    }
+  }, [hash])
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <Menu />
